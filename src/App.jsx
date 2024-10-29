d252a4d9dfa911b2fb073ee0e87bc8c7c29c0fb1
camdelay16 <camdelay16@gmail.com>
2024-10-29 15:10:32 -0400

    made weather forecast component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,62 @@
-
+import WeatherForecast from './components/WeatherForecast/WeatherForecast';
 import './App.css'
 
 const App = () => {
+  const weatherForecasts = [
+    {
+      day: 'Mon',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/day.svg',
+      imgAlt: 'sun icon',
+      conditions: 'sunny',
+      time: 'Morning',
+      id: 1,
+    },
+    {
+      day: 'Tue',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/night.svg',
+      imgAlt: 'moon icon',
+      conditions: 'clear',
+      time: 'Night',
+      id: 2,
+    },
+    {
+      day: 'Wed',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/stormy.svg',
+      imgAlt: 'clouds with lightning icon',
+      conditions: 'stormy',
+      time: 'All Day',
+      id: 3,
+    },
+    {
+      day: 'Thu',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-day_t7ckxp.svg',
+      imgAlt: 'sun overcast by clouds icon',
+      conditions: 'overcast',
+      time: 'Evening',
+      id: 4,
+    },
+    {
+      day: 'Fri',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-night.svg',
+      imgAlt: 'moon overcast by clouds icon',
+      conditions: 'cloudy',
+      time: 'Night',
+      id: 5,
+    },
+  ];
 
   return (
     <>
+      <h1>Local Weather</h1>
+      <section>
+        {weatherForecasts.map((weatherForecast) => (
+          <WeatherForecast 
+          key={weatherForecast.id}
+          {...weatherForecast}/>
+        ))}
+      </section>
     </>
+
   )
 }
 
